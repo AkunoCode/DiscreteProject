7b43ebb3530286ad74f58e6aea2dcc3736707a6d
John Leo D. Echevaria <echevariajohnleo@gmail.com>
2023-11-25 21:47:42 +0800

    Made calculator static

--- src/main/java/com/discrete/finalsproject/Models/Calculator.java
+++ src/main/java/com/discrete/finalsproject/Models/Calculator.java
@@ -3,108 +3,107 @@ package com.discrete.finalsproject.Models;
 import java.util.ArrayList;
 
 public class Calculator {
-    private ArrayList<Double> data;
-    private int numberOfData;
-    private double mean, sampleStandardDeviation, sampleVariance,
+    private static ArrayList<Double> data;
+    private static int numberOfData;
+    private static double mean, sampleStandardDeviation, sampleVariance,
             sumOfXMinusMeanSquared, populationStandardDeviation, populationVariance;
-    private ArrayList<Double> xMinusMean, xMinusMeanSquared;
+    private static ArrayList<Double> xMinusMean, xMinusMeanSquared;
 
-    private void setData(ArrayList<Double> data){
-        this.data = data;
-        this.numberOfData = data.size();
+    public static void setData(ArrayList<Double> inputData){
+        data = inputData;
+        numberOfData = data.size();
     }
-    private ArrayList<Double> getData(){
-        return this.data;
+    public static ArrayList<Double> getData(){
+        return data;
     }
-    private int getNumberOfData(){
-        return this.numberOfData;
+    public static int getNumberOfData(){
+        return numberOfData;
     }
 
     // TODO: Calculate mean - Nicole
-    private void calculateMean(){
+    public static void calculateMean(){
         // Set the value of mean
         // this.mean =
     }
 
     // Get mean
     public double getMean(){
-        return this.mean;
+        return mean;
     }
 
     // TODO: Calculate sample variance - Adrielle
-    private void calculateVariance(){
+    public static void calculateVariance(){
         // Set the value of variance
         // this.variance =
     }
 
     // Get variance
     public double getVariance(){
-        return this.sampleVariance;
+        return sampleVariance;
     }
 
     // TODO: Calculate sample standard deviation - Adrielle
-    private void calculateStandardDeviation(){
+    public static void calculateStandardDeviation(){
         // Set the value of standard deviation
         // this.standardDeviation =
     }
 
     // Get standard deviation
     public double getStandardDeviation(){
-        return this.sampleStandardDeviation;
+        return sampleStandardDeviation;
     }
 
     // TODO: Calculate population variance - Nicole
-    private void calculatePopulationVariance(){
+    public static void calculatePopulationVariance(){
         // Set the value of population variance
         // this.populationVariance =
     }
 
     // Get population variance
     public double getPopulationVariance(){
-        return this.populationVariance;
+        return populationVariance;
     }
 
     // TODO: Calculate population standard deviation - Nicole
-    private void calculatePopulationStandardDeviation(){
+    public static void calculatePopulationStandardDeviation(){
         // Set the value of population standard deviation
         // this.populationStandardDeviation =
     }
 
     // Get population standard deviation
     public double getPopulationStandardDeviation(){
-        return this.populationStandardDeviation;
+        return populationStandardDeviation;
     }
 
     // TODO: Calculate sum of x minus mean squared - Johann
-    private void calculateSumOfXMinusMeanSquared(){
+    public static void calculateSumOfXMinusMeanSquared(){
         // Set the value of sum of x minus mean squared
         // this.sumOfXMinusMeanSquared =
     }
 
     // Get sum of x minus mean squared
     public double getSumOfXMinusMeanSquared(){
-        return this.sumOfXMinusMeanSquared;
+        return sumOfXMinusMeanSquared;
     }
 
     // TODO: caclulate X minus mean - Leo
-    private void calculateXMinusMean(){
-        // Set the value of x minus mean
-        // this.xMinusMean =
+    public static void calculateXMinusMean(){
+
     }
 
     // Get x minus mean
     public ArrayList<Double> getXMinusMean(){
-        return this.xMinusMean;
+        return xMinusMean;
     }
 
     // TODO: calculate X minus mean squared - Leo
-    private void calculateXMinusMeanSquared(){
+    public static void calculateXMinusMeanSquared(){
         // Set the value of x minus mean squared
         // this.xMinusMeanSquared =
     }
 
     // Get x minus mean squared
-    public ArrayList<Double> getXMinusMeanSquared(){
-        return this.xMinusMeanSquared;
+    public static ArrayList<Double> getXMinusMeanSquared(){
+        return xMinusMeanSquared;
     }
 }
