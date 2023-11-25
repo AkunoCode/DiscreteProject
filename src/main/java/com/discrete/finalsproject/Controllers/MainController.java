4fe9a4954940cd8e16caf4215ec7dfc8d0b9da32
John Leo D. Echevaria <echevariajohnleo@gmail.com>
2023-11-25 21:47:18 +0800

    Added inputfield, buttons, and styled

--- src/main/java/com/discrete/finalsproject/Controllers/MainController.java
+++ src/main/java/com/discrete/finalsproject/Controllers/MainController.java
@@ -1,9 +1,58 @@
 package com.discrete.finalsproject.Controllers;
 
+import com.discrete.finalsproject.Models.Calculator;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.util.ArrayList;
 
 public class MainController {
     // TODO: Table [Columns: Data, Data - Mean, (Data - Mean)^2] - Johann
     // TODO: Histogram/Graph - Leo
+    @FXML
+    private TextField inputField;
+
+    @FXML
+    private Button populationButton;
+
+    @FXML
+    private Button sampleButton;
+
+    private ArrayList<Double> getInputData(){
+        // Convert commas to spaces and split by spaces. If there is more than one space, convert it to one space.
+        String[] inputString = inputField.getText().replaceAll(",", " ").replaceAll("\\s+", " ").split(" ");
+        ArrayList<Double> inputData = new ArrayList<>();
+
+        // Convert the string array to double array
+        for (String s : inputString) {
+            inputData.add(Double.parseDouble(s));
+        }
+        return inputData;
+    }
+
+    @FXML
+    private void onSampleButtonClicked(){
+        // check if text field has non-numeric characters
+        if (!inputField.getText().matches("[0-9, ]+")) {
+            System.out.println("Invalid input");
+            inputField.clear();
+            return;
+        }
+        Calculator.setData(getInputData());
+        System.out.println(Calculator.getData());
+    }
+
+    @FXML
+    private void onPopulationButtonClicked(){
+        // check if text field has non-numeric characters
+        if (!inputField.getText().matches("[0-9, ]+")) {
+            System.out.println("Invalid input");
+            inputField.clear();
+            return;
+        }
+        Calculator.setData(getInputData());
+        System.out.println(Calculator.getData());
+    }
+
 }
--- src/main/java/com/discrete/finalsproject/MainApplication.java
+++ src/main/java/com/discrete/finalsproject/MainApplication.java
@@ -3,6 +3,7 @@ package com.discrete.finalsproject;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -11,9 +12,11 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("Main.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("FXMLs/Main.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        scene.setFill(Color.TRANSPARENT); // Make the background transparent
         stage.initStyle(StageStyle.UNDECORATED); // Remove the titleBar
+        stage.initStyle(StageStyle.TRANSPARENT); // Make the window transparent
         stage.setScene(scene);
         stage.show();
     }
